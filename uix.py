7c2af96f9b57db609a889ba9de2535995e74b930
Famel1x <98748544+Famel1x@users.noreply.github.com>
2023-11-17 21:40:30 +0300

    ф

---
+++ uix.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+def main(page: ft.Page) -> ft.Page:
+    """
+    Главная функция Интерфейса.
+    
+        Что должно выводить?:
+            1)
+    """
+
+    
+
+
+
+
+ft.app(target = main)
