bbd6d1ce3ea63f359efa992a2ad715c8572068e9
Famel1x <98748544+Famel1x@users.noreply.github.com>
2023-11-18 11:41:16 +0300

    ф

--- vibe/vibe.py
+++ vibe/vibe.py
@@ -1,15 +1,21 @@
 
 import pandas as pd
 from collections import Counter
+import re
+
+par = re.findall(r'\w+', open('vibe/bakroti.txt').read().lower())
+print(Counter(par).most_common(10))
 
 
-with open("vibe/vibe.txt", encoding='utf-8') as f:
-    lines = f.readlines()
+# with open("vibe/bakroti.txt", encoding='utf-8') as f:
+#     lines = f.readlines()
 
-c = Counter(lines)
+# for i in range(len(lines)):
+#     lines[i]=lines[i].rstrip()
+# c = Counter(lines)
 
 
-# for key, value in c.items():
-#     print("{0}: {1}".format(key, value))
-print(c.most_common(3))
+# # for key, value in c.items():
+# #     print("{0}: {1}".format(key, value))
+# print(c.most_common(10))
 
