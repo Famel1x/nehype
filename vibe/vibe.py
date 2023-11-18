fdf3f678d60dc87ef5b66dea073310e24ffd6cc5
Famel1x <98748544+Famel1x@users.noreply.github.com>
2023-11-18 12:13:45 +0300

    a

--- vibe/vibe.py
+++ vibe/vibe.py
@@ -2,20 +2,36 @@
 import pandas as pd
 from collections import Counter
 import re
+from pprint import pprint
+from tabulate import tabulate
 
-par = re.findall(r'\w+', open('vibe/bakroti.txt').read().lower())
-print(Counter(par).most_common(10))
+# par = re.findall(r'\w+', open('vibe/bakroti.txt').read().lower())
+# print(Counter(par).most_common(10))
+with open("vibe/name_bonkroti.txt", encoding='utf-8') as f:
+    bank = f.readlines()
+
+with open("vibe/name_norm.txt", encoding='utf-8') as f:
+    norm = f.readlines()
+
+norm = Counter(norm).most_common(20)
+bank = Counter(bank).most_common(20)
+
+result = []
+
+for i in range(len(norm)):
+    a = norm[i]
+    for i in range(len(bank)):
+        if a[0] in bank[i]:
+            id= bank[i]
+            proc = id[1]/(a[1]/100)
+            result.append([a[0].rstrip(), a[1], id[1], proc])
 
 
-# with open("vibe/bakroti.txt", encoding='utf-8') as f:
-#     lines = f.readlines()
-
-# for i in range(len(lines)):
-#     lines[i]=lines[i].rstrip()
-# c = Counter(lines)
+print(tabulate((result),headers=['Name', "Norm", "Bank", "Proc"], tablefmt="orgtbl"))
 
 
-# # for key, value in c.items():
-# #     print("{0}: {1}".format(key, value))
-# print(c.most_common(10))
+
+# for key, value in c.items():
+#     print("{0}: {1}".format(key, value))
+
 
